c3fd60544af667d5a075c0e72e37cdbb26520e38
mflorhernandez <mflorhernandez330copia@gmail.com>
2025-06-17 19:43:37 -0300

    Cambios en pages

--- components/pages.js
+++ components/pages.js
@@ -1,9 +1,9 @@
 export const pages = [
-    { title: "Home", href: "../index.html", requiresLogin: false },
+    { title: "Home", href: "./index.html", requiresLogin: false },
     { title: "Ropa", href: "../Categorias/Ropa.html", requiresLogin: false },
     { title: "Tecnología", href: "../Categorias/Tecnologia.html", requiresLogin: false },
     { title: "Hogar", href: "../Categorias/Hogar.html", requiresLogin: false },
-    { title: "Login", href: "../login.html", requiresLogin: false },
-    { title: "Registro", href: "../registrar.html", requiresLogin: false },
-    { title: "Logout", href: "../login.html", requiresLogin: true }
+    { title: "Login", href: "./login.html", requiresLogin: false },
+    { title: "Registro", href: "./registrar.html", requiresLogin: false },
+    { title: "Logout", href: "./login.html", requiresLogin: true }
 ];
