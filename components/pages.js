4f4859f54cf20e82ad4e35bda75b0e6e44a361ab
mflorhernandez <mflorhernandez330copia@gmail.com>
2025-06-17 19:29:31 -0300

    navbar

--- components/pages.js
+++ components/pages.js
@@ -1,5 +1,5 @@
 export const pages = [
-    { title: "Home", href: "../index.html", requiresLogin: false },
+    { title: "Home", href: "./index.html", requiresLogin: false },
     { title: "Ropa", href: "../Categorias/Ropa.html", requiresLogin: false },
     { title: "Tecnología", href: "../Categorias/Tecnologia.html", requiresLogin: false },
     { title: "Hogar", href: "../Categorias/Hogar.html", requiresLogin: false },
